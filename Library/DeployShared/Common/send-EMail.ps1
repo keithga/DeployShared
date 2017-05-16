9b220578a7c7b2a702102c58782115d3faed991f
Keith Garner <keith.garner@hotmail.com>
2017-05-16 13:33:39 -0500

    Send E-Mail

---
+++ Library/DeployShared/Common/send-EMail.ps1
@@ -0,0 +1,26 @@
+﻿
+<#
+
+This file is a work in progress
+
+$secpasswd = ConvertTo-SecureString "XXXXXXX" -AsPlainText -Force
+
+$SendArgs = @{
+    From = 'xxxx@keithga.com'
+    To = '425xxxxxxx@txt.att.net'
+    SMTPServer = 'xxx.com'
+    UseSSL = $True
+    Port = 587
+    Credential = New-Object System.Management.Automation.PSCredential ("XXXX@xxxxx.com", $secpasswd)
+    Embedded = @{
+        Credential = New-Object System.Management.Automation.PSCredential ("xxxx@xxxx.com", $secpasswd)
+        MyPWD = $secpasswd
+    }
+    MyPassword = $secpasswd
+}
+
+Send-MailMessage -body "This is a test message" -Subject "Update" @SendArgs
+
+#>
+
+
---
+++ Library/DeployShared/Common/update-Password.ps1
@@ -0,0 +1,41 @@
+﻿<#
+Update the password in a CLIXML file.
+#>
+
+function Parse-HashTable {
+    param( 
+        [HashTable] $Hash,
+        [string] $password
+    )
+
+    $MyKeys = $Hash.Keys -as [string[]]
+    foreach ( $Key in $MyKeys ) {
+        write-verbose "Looking at $($hash[$key])"
+        if ( $Hash[$key].gettype().Name -eq 'Hashtable' ) {
+            Parse-HashTable -hash $Hash[$Key] -password $Password
+        }
+        elseif ( $Hash[$key].gettype().Name -eq 'PSCredential' ) {
+            $newPassword = ConvertTo-SecureString -force -AsPlainText $password
+            $hash[$key] = New-Object System.Management.Automation.PSCredential ($hash[$key].UserName, $NewPassword)
+        }
+        elseif ( $Hash[$key].gettype().Name -eq 'SecureString' ) {
+            $hash[$key] = ConvertTo-SecureString -Force -AsPlainText $password
+        }
+    }
+}
+
+Update-Password {
+    [cmdletbinding()]
+    PARAM(
+        [parameter(Mandatory=$true)]
+        $path,
+        [parameter(Mandatory=$true)]
+        $Password
+    )
+
+    $hash = Import-Clixml -Path $path
+    $hash | out-string | Write-Verbose
+    Parse-HashTable -Hash $hash -password $Password
+    move -Path $path -Destination ( $path + ".old" ) -Force
+    $hash | Export-Clixml -Path $Path
+}
